e0481121c155b5c1421bf48173e33e66a42f11aa
alireza ranjbar <rasampostman@gmail.com>
2025-02-09 08:53:06 +0330

    ar

---
+++ Eshop.Data/Migrations/20250209052236_ar.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rasam.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                schema: "Model",
+                table: "Store",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                schema: "Model",
+                table: "Store");
+        }
+    }
+}
