4e7ac664d623451f235b9b3dfa661e779bdaae76
alireza ranjbar <rasampostman@gmail.com>
2025-03-05 11:37:09 +0330

    ar

--- Eshop.Mapper/Models/VendorProfile.cs
+++ Eshop.Mapper/Models/VendorProfile.cs
@@ -14,7 +14,8 @@ namespace Eshop.Mapper.Models
             CreateMap<VendorEntity, VendorUserDTO>()
                 .ForMember(des => des.UserName, option => option.MapFrom(src => src.User != null ? src.User.UserName : string.Empty))
                 .ForMember(des => des.Email, option => option.MapFrom(src => src.User != null ? src.User.Email : string.Empty))
-                .ForMember(des => des.PhoneNumber, option => option.MapFrom(src => src.User != null ? src.User.PhoneNumber : string.Empty));
+                .ForMember(des => des.PhoneNumber, option => option.MapFrom(src => src.User != null ? src.User.PhoneNumber : string.Empty))
+                .ReverseMap();
 
             CreateMap<VendorUserDTO, AddUserDTO>();
 
